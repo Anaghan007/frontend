147e6456e3064c667541c4ade5c132da6779a5eb
Anaghan007 <anaghan245@gmail.com>
2025-01-02 09:52:37 +0530

    2-1-25

---
+++ jQuery/02-01-25/jquery.js
@@ -0,0 +1,131 @@
+/* JQuery Html Method */
+
+/* Get Method */
+
+{
+    // $(function () {
+    //     $('#btn').click(function () {
+    //        console.log( $('#demo').html());
+    //     //    alert($('#demo').html())
+    //     })
+    // })
+
+
+    // $(function () {
+    //     $('#btn').click(function () {
+    //        console.log( $('#demo').text());
+    //     //    alert($('#demo').html())
+    //     })
+    // })
+
+    // $(function () {
+    //     $('#btn').click(function () {
+    //        console.log( $('#text').val());
+    //     //    alert($('#demo').html())
+    //     })
+    // })
+}
+
+/* jQuery Set Method */
+
+{
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('#demo').html('<div>This is Element</div')
+    //     })
+    // })
+
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('#demo').text('Hello World!!!!!!')
+    //     })
+    // })
+
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('#text').val('Hello World!')
+    //     })
+    // })
+
+    // $(function(){
+    //     $('#btn').click(function(){
+    //         $('#text').attr({"class":"box"})
+    //     })
+    // })
+
+    // $(function(){
+    //     $("#styles").click(function(){
+    //         $('.box').addClass("box-1")
+    //     })
+    // })
+
+    // $(function(){
+    //     $("#styles").click(function(){
+    //         $('.box-1').removeClass("box")
+    //     })
+    // })
+
+    // $(function(){
+    //     $("#styles").click(function(){
+    //         $('.box').toggleClass("box-1")
+    //     })
+    // })
+
+}
+
+/* jQuery add method of behind and before content */
+
+/*
+
+append
+prepend
+after
+before
+
+*/
+
+// {
+//     $(function(){
+//         $('#btn').click(function(){
+//             $('#heading').append('Hello World!!')
+//         })
+//     })
+// }
+
+// {
+//     $(function(){
+//         $('#btn').click(function(){
+//             $('#heading').prepend('Hello World!!')
+//         })
+//     })
+// }
+
+// {
+//     $(function(){
+//         $('#btn').click(function(){
+//             $('#heading').after('Hello World!!')
+//         })
+//     })
+// }
+
+// {
+//     $(function(){
+//         $('#btn').click(function(){
+//             $('#heading').before('Hello World!!')
+//         })
+//     })
+// }
+
+/* remove content */
+
+$(function(){
+    $('#btn').click(function(){
+        $('p').remove('#para')
+    })
+})
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('body').empty()
+//     })
+// })
