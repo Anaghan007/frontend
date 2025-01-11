7555374365cfde677ea397d1db3db0476e9fd772
Anaghan007 <anaghan245@gmail.com>
2025-01-11 10:05:56 +0530

    11-01-25

---
+++ JS Practis/script.js
@@ -0,0 +1,39 @@
+/* Array.At() */
+
+// let array = [1,2,3,4,5,6,7]
+// console.log(array);
+
+// let arrayvalues = array.at(2)
+// console.log(arrayvalues)
+
+/* Array.every() , Array.some() */
+
+// let array = [1,2,3,4,5,6,7]
+
+// let everyValues = array.every((num) => num > 5)
+// console.log(everyValues);
+
+// let someValues = array.some((num) => num > 5)
+// console.log(someValues);
+
+
+/* Array.reduce() */
+
+// let num = [10, 25, 2, 3, 5, 7]
+
+// let reduce = num.reduce((PreNum , NextNum) => PreNum + NextNum, 0)
+// console.log(reduce);
+
+// let reduced = num.reduceRight((PreNum , NextNum) => PreNum * NextNum, 1)
+// console.log(reduced);
+
+
+/* Array.sort() */
+
+// let sort = ['Html' , 'Css' , 'Bootstrap' , 'Sql' , 'Javascript']
+// let sortValues = sort.sort()
+// console.log(sortValues);
+
+// let sortNum = [5,10,20,15,30,25,45,35,40]
+// let sortNumValues = sortNum.sort((a , b) => a - b)
+// console.log(sortNumValues);
