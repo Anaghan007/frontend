d8621cb2d0f91d673621f821e773264616358b78
Anaghan007 <anaghan245@gmail.com>
2025-01-13 09:50:07 +0530

    13-01-25

--- JS Practis/script.js
+++ JS Practis/script.js
@@ -37,3 +37,57 @@
 // let sortNum = [5,10,20,15,30,25,45,35,40]
 // let sortNumValues = sortNum.sort((a , b) => a - b)
 // console.log(sortNumValues);
+
+
+// JavaSCript JSON
+
+[
+    {
+        ProductName:"Tv",
+        ProductColor:"Black",
+        ProductCompny:"Samsung",
+        ProductPrice:25000,
+        ProductQuality:"HD",
+        ProductStock:"Availabel"
+    },
+    {
+        ProductName:"Laptop",
+        ProductColor:"Gray",
+        ProductCompny:"Dell",
+        ProductPrice:15000,
+        ProductQuality:"FHD",
+        ProductStock:"NotAvailabel"
+    },
+    {
+        ProductName:"Freez",
+        ProductColor:"White",
+        ProductCompny:"LG",
+        ProductPrice:85000,
+        ProductQuality:"Super",
+        ProductStock:"Availabel"
+    },
+    {
+        ProductName:"Tv",
+        ProductColor:"Black",
+        ProductCompny:"Redmi",
+        ProductPrice:35000,
+        ProductQuality:"FHD+",
+        ProductStock:"NotAvailabel"
+    },
+    {
+        ProductName:"Laptop",
+        ProductColor:"Silver",
+        ProductCompny:"HP",
+        ProductPrice:45000,
+        ProductQuality:"HD+",
+        ProductStock:"Availabel"
+    },
+    {
+        ProductName:"Freez",
+        ProductColor:"Gray",
+        ProductCompny:"Toshiba",
+        ProductPrice:55000,
+        ProductQuality:"Super",
+        ProductStock:"NotAvailabel"
+    }
+]
